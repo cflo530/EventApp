7c8699f17604c96e287564916a8d34739e5caf02
Carlos Flores <57191245+cflo530@users.noreply.github.com>
2021-06-03 00:05:01 -0700

    Add files via upload

--- database/setup.js
+++ database/setup.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = 'mongodb://localhost:27017/eventapp';
+const connectionString = 'mongodb+srv://cflo530:event123@cluster0.x6mkf.mongodb.net/eventapp?retryWrites=true&w=majority';
 
 module.exports = () => {
     mongoose.connect(connectionString,{
